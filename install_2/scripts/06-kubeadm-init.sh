1578eafb08416b3279196a5f0a59780d436ce1e5
atulsingh0 <victimizeit@gmail.com>
2022-12-08 07:35:39 -0500

    main | added join script

--- install_2/scripts/06-kubeadm-init.sh
+++ install_2/scripts/06-kubeadm-init.sh
@@ -4,10 +4,6 @@ sudo kubeadm init --pod-network-cidr 10.244.0.0/16 \
   --apiserver-advertise-address=192.168.56.11 \
   --kubernetes-version 1.24.0
 
-mkdir -p $HOME/.kube
-sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-sudo chown $(id -u):$(id -g) $HOME/.kube/config
-
 echo "" >/vagrant/k8s-join-script.sh
 echo '#!/bin/bash' >>/vagrant/k8s-join-script.sh
 sudo kubeadm token create --print-join-command >>/vagrant/k8s-join-script.sh
